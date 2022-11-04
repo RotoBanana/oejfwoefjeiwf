54a54142da446737b5b3a3e6d842c713519c45f1
RotoBanana <62190339+RotoBanana@users.noreply.github.com>
2022-11-04 20:14:33 +0200

    Update Email_Sender.py

--- Email_Sender.py
+++ Email_Sender.py
@@ -8,7 +8,6 @@ import pytz
 
 times_sent = 1
 
-#BUG: Change Local time date to Israel, for the VPS.
 #OPTIMIZE: if the hour is not between 7:00-18:00, dont let it check every minute again and again. just let it sleep.
 
 def email_sender_to_military(times_sent, now_time): #Putting it in a function for comfort
@@ -56,76 +55,81 @@ def current_time():
 
     return now_time
 
-#Random Hour
-ran_hour = random.randint(7,18)
-ran_minute = random.randint(0, 60)
-if ran_hour == 18: ran_minute = 0
+def random_times():
+    #First hour Randomizer
+    ran_hour_0 = random.randint(7,18)
+    ran_minute_0 = random.randint(0, 60)
+    if ran_hour_0 == 18: ran_minute_0 = 0
+    if int(ran_minute_0) < 10: ran_minute_0 = '0' + str(ran_minute_0)
 
-#Print Random Hour + Minute
-ran_time = str(ran_hour) + ':' + str(ran_minute)
-print("The RANDOM time is: %s" % (ran_time))
+    ran_time_0 = str(ran_hour_0) + ':' + str(ran_minute_0)
+
+    #Second hour Randomizer
+    #If 1st hour is BIGGER by 3 than 0st hour OR if 1st hour is SMALLER by 3 than 1st hour, go on.
+    while True: 
+        ran_hour_1 = random.randint(7,18)
+        ran_minute_1 = random.randint(0, 60)
+        if ran_hour_1 == 18: ran_minute_1 = 0
+        if int(ran_minute_1) < 10: ran_minute_1 = '0' + str(ran_minute_1)
+
+        if (ran_hour_1 >= ran_hour_0 + 3 or ran_hour_1 <= ran_hour_0 - 3):
+            break
+        else:
+            continue
+
+    ran_time_1 = str(ran_hour_1) + ':' + str(ran_minute_1)
+
+    #Third hour Randomizer
+    #If 2nd hour is BIGGER by 3 than 0st hour AND if 2nd hour is BIGGER than 1st hour, go on. Example: 0st: 8:00, 1st: 12:00, 2nd: 16:00
+    #If 2nd hour is SMALLER by 3 than 0st hour AND if 2nd hour is SMALLER than 1st hour, go on.
+    while True:
+        ran_hour_2 = random.randint(7,18)
+        ran_minute_2 = random.randint(0, 60)
+        if ran_hour_2 == 18: ran_minute_2 = 0
+        if int(ran_minute_2) < 10: ran_minute_2 = '0' + str(ran_minute_2)
+
+        if ((ran_hour_2 >= ran_hour_0 + 3 and ran_hour_2 >= ran_hour_0 + 3)
+        or (ran_hour_2 <= ran_hour_0 - 3 and ran_hour_2 <= ran_hour_0 - 3)):
+            break
+        else:
+            continue
+
+    ran_time_2 = str(ran_hour_2) + ':' + str(ran_minute_2)
+
+    #Print random times
+    print("""Random times are:
+    1st: %s
+    2nd %s
+    3rd %s"""
+    % (ran_time_0, ran_time_1, ran_time_2))
+
+    return ran_time_0, ran_minute_1, ran_minute_2
 
 
-#First hour Randomizer
-ran_hour_0 = random.randint(7,18)
-ran_minute_0 = random.randint(0, 60)
-if ran_hour_0 == 18: ran_minute_0 = 0
-if int(ran_minute_0) < 10: ran_minute_0 = '0' + str(ran_minute)
 
-ran_time_0 = str(ran_hour_0) + ':' + str(ran_minute_0)
-
-#Second hour Randomizer
-#If 1st hour is BIGGER by 3 than 0st hour OR if 1st hour is SMALLER by 3 than 1st hour, go on.
-while True: 
-    ran_hour_1 = random.randint(7,18)
-    ran_minute_1 = random.randint(0, 60)
-    if ran_hour_1 == 18: ran_minute_1 = 0
-    if int(ran_minute_1) < 10: ran_minute_1 = '0' + str(ran_minute)
-
-    if (ran_hour_1 >= ran_hour_0 + 3 or ran_hour_1 <= ran_hour_0 - 3):
-        break
-    else:
-        continue
-
-ran_time_1 = str(ran_hour_1) + ':' + str(ran_minute_1)
-
-#Third hour Randomizer
-#If 2nd hour is BIGGER by 3 than 0st hour AND if 2nd hour is BIGGER than 1st hour, go on. Example: 0st: 8:00, 1st: 12:00, 2nd: 16:00
-#If 2nd hour is SMALLER by 3 than 0st hour AND if 2nd hour is SMALLER than 1st hour, go on.
-while True:
-    ran_hour_2 = random.randint(7,18)
-    ran_minute_2 = random.randint(0, 60)
-    if ran_hour_2 == 18: ran_minute_2 = 0
-    if int(ran_minute_2) < 10: ran_minute_2 = '0' + str(ran_minute)
-
-    if ((ran_hour_2 >= ran_hour_0 + 3 and ran_hour_2 >= ran_hour_0 + 3)
-     or (ran_hour_2 <= ran_hour_0 - 3 and ran_hour_2 <= ran_hour_0 - 3)):
-        break
-    else:
-        continue
-
-ran_time_2 = str(ran_hour_2) + ':' + str(ran_minute_2)
-
-
-#Print random times
-print("""Random times are:
-1st: %s
-2nd %s
-3rd %s"""
-% (ran_time_0, ran_time_1, ran_time_2))
 
 
 #timer
-while True:
-    now_time = current_time()
-    if ((now_time == ran_time_0) or
-        (now_time == ran_time_1) or
-        (now_time == ran_time_2) or
-        (now_time == now_time)):
 
-        email_sender_to_military(times_sent, now_time)
-        times_sent += 1
-        time.sleep(60)
+
+while True:
+    checker = 0
+    ran_time_0, ran_time_1, ran_time_2 = random_times()
+
+    while checker >= 3:
+        now_time = current_time()
+        
+        if ((now_time == ran_time_0) or
+            (now_time == ran_time_1) or
+            (now_time == ran_time_2) or
+            (now_time == now_time)):
+
+            email_sender_to_military(times_sent, now_time)
+            checker += 1
+            times_sent += 1
+            time.sleep(60)
+        
+        else:
+            time.sleep(60)
     
-    else:
-        time.sleep(60)
+    #TO DO: EXIT SECOND LOOP HERE, AND MAKE IT SLEEP UNTIL THE NEXT DAY, THEN THE LOOP WILL GO AGAIN.
